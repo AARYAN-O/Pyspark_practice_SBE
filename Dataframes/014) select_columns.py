fc9268d25ae4556da0ac409764e5fb0f6d670174
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-30 18:10:09 +0530

    Create 014) select_columns.py

---
+++ Dataframes/014) select_columns.py
@@ -0,0 +1,20 @@
+data=[('Peter','Parker',12),('Tony','Stark',14)]
+
+rdd=spark.sparkContext.parallelize(data)
+
+column_names=['firstname','lastname','Age']
+df=spark.createDataFrame(data,column_names)
+
+# 1)
+df.select('firstname','Age').show()
+
+# 2) 
+df.select(df.firstname,df.Age).show()
+
+# 3) 
+df.select(df['firstname'],df['Age']).show()
+
+# 4)  using col
+
+from pyspark.sql.functions import col
+df.select(col('firstname'),col('lastname')).show()
