1e63522046baa51d6b4b6737ab297605ba26265a
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-30 15:04:04 +0530

    Update and rename setting_the_name_of_columns_using_toDF.py to 008) setting_the_name_of_columns_using_toDF.py

--- Dataframes/008) setting_the_name_of_columns_using_toDF.py
+++ Dataframes/008) setting_the_name_of_columns_using_toDF.py
@@ -3,6 +3,8 @@ rdd=spark.sparkContext.parallelize(data)
 
 from pyspark.sql.types import StructType, StringType,StructField
 
+# toDF is where you can set the names of the column.
+# pass a list of strings (which correspond to the column names here)
 df=rdd.toDF(['Name','Last Name','Age'])
 
 df.show()
