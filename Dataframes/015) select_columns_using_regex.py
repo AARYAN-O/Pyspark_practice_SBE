b9cc6bf285d5e45760f126aac3f0088b9cad9517
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-07-01 18:31:41 +0530

    Update 015) select_columns_using_regex.py

--- Dataframes/015) select_columns_using_regex.py
+++ Dataframes/015) select_columns_using_regex.py
@@ -1,1 +1,13 @@
-# complete this 
+#Note that since spark is written in scala , it relies on the scala regex (which is very similiar to java)
+
+data=[('Peter',20),('Tony',40)]
+
+column_names=['Name','Age']
+
+rdd=spark.sparkContext.parallelize(data)
+
+df=rdd.toDF(schema)
+
+df.select(df.colRegex("`^.*name*`")).show()
+
+
