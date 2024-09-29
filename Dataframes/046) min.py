71564514bd8b90ca0271ce2e32f3ab61fc4f08ba
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-09-29 15:02:29 +0530

    Update 046) min.py

--- Dataframes/046) min.py
+++ Dataframes/046) min.py
@@ -4,4 +4,4 @@ from pyspark.sql.functions import col
 df=spark.read.format("csv").option("header","true").load("dbfs:/FileStore/superstore.csv")
 df=df.withColumn("Profit",col("Profit").cast("Integer"))
 
-df.groupBy("Country").min().show()
+df.groupBy("Country").max().show()
