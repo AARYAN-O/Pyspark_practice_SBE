0b033a9e4e494fab3e8f797e40057a2871d4503f
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-09-29 10:18:17 +0530

    Update 035) drop duplicates of selected columns.py

--- Dataframes/035) drop duplicates of selected columns.py
+++ Dataframes/035) drop duplicates of selected columns.py
@@ -10,3 +10,19 @@ df=spark.createDataFrame(data,schema)
 df1=df.dropDuplicates(['City','Favourite_Number'])
 
 df1.show()
+
+# -----------------------
+# Dropping duplicates of one column only 
+
+from pyspark.sql.types import StructType,StructField,IntegerType,StringType
+
+schema=StructType([StructField('Person',StringType(),True),StructField('Roll_No',IntegerType(),True),StructField('City',StringType(),True),
+                   StructField('Favourite_Number',IntegerType(),True)])
+
+data=[('Person1',12,'Los Angles',144),('Person2',14,'Miami',132),('Person3',14,'Miami',132),('Person2',14,'Miami',132)]
+
+df=spark.createDataFrame(data,schema)
+
+df1=df.dropDuplicates(['Person'])
+
+df1.show()
