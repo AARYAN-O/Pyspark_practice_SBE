7ddad850cc7e0a13290f559f1769eca3a0f3c61a
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-09-29 15:37:41 +0530

    Create 048) mean_and_avg.py

---
+++ Dataframes/048) mean_and_avg.py
@@ -0,0 +1,8 @@
+from pyspark.sql.functions import col
+
+df=spark.read.format("csv").option("header","true").load("dbfs:/FileStore/superstore.csv")
+
+df.display()
+df=df.withColumn("Profit",col("Profit").cast("Integer"))
+
+df.groupBy("Country").mean().display()
