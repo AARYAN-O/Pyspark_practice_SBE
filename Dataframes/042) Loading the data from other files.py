4997fbc4476d5c3d15b9b030b0892961f20ec86f
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-09-29 13:41:10 +0530

    Update 042) Loading the data from other files.py

--- Dataframes/042) Loading the data from other files.py
+++ Dataframes/042) Loading the data from other files.py
@@ -1,5 +1,6 @@
 # For uploading the files,
 # click on catalog => dbfs => Upload the file inside the filestore => click on the down arrow => and then copy path
+# Note: spark is a keyword in pyspark
 
 df=spark.read.format("csv").option("header","true").load("dbfs:/FileStore/bank_customer.csv")
 df.show()
