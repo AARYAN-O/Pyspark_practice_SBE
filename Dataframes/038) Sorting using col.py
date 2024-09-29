38f77f856533f25ad5cf9f8afde86204553a4ddb
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-09-29 10:43:41 +0530

    Update 038) Sorting using col.py

--- Dataframes/038) Sorting using col.py
+++ Dataframes/038) Sorting using col.py
@@ -12,3 +12,22 @@ df=spark.createDataFrame(data,schema)
 df1=df.sort(col('Person'))
 
 df1.show()
+
+-----------------------------------
+
+# using col in sort 
+
+from pyspark.sql.types import StringType,IntegerType,StructType,StructField
+from pyspark.sql.functions import col
+
+schema=StructType([StructField('Person',StringType(),True),StructField('Roll_No',IntegerType(),True)])
+
+data=[('Person2',12),('Person1',14)]
+
+df=spark.createDataFrame(data,schema)
+
+df2=df.sort(col('Person'),ascending=False)
+
+df2.show()
+
+
