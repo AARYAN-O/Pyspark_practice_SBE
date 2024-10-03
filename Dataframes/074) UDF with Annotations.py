9a4720e93802c81885e7f4662a6967977a2a4cfe
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-03 13:29:04 +0530

    Update 074) UDF with Annotations.py

--- Dataframes/074) UDF with Annotations.py
+++ Dataframes/074) UDF with Annotations.py
@@ -17,3 +17,10 @@ def convertcase(s):
 df=spark.read.format("csv").option("header","true").load("dbfs:/FileStore/movies.csv")
 df.withColumn("new_column",convertcase("title")).display()
 
+# What are the disadvantages of using UDFs?
+# Its always best practice to check for null inside a UDF function rather than checking for null outside.
+# In any case, if you can’t do a null check in UDF at lease use IF or CASE WHEN to check for null and call UDF conditionally.
+# UDFs are a black box to PySpark hence it can’t apply optimization and you will lose all the optimization PySpark does on Dataframe/Dataset.
+# When possible you should use Spark SQL built-in functions as these functions provide optimization. Consider creating UDF only when the existing built-in SQL 
+# function doesn’t have it.
+
