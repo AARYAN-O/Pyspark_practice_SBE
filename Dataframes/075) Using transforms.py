148e67274fb63c5092f4bcd3e7634034be14ec35
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-03 15:16:14 +0530

    Update 075) Using transforms.py

--- Dataframes/075) Using transforms.py
+++ Dataframes/075) Using transforms.py
@@ -9,16 +9,44 @@
 # THE FORM OF ARRAYS.
 
 
-from pyspark.sql.functions import lit,concat
+# from pyspark.sql.functions import lit,concat,length
 
-df=spark.read.format("csv").option("header","true").load("dbfs:/FileStore/movies.csv")
+# df=spark.read.format("csv").option("header","true").load("dbfs:/FileStore/movies.csv")
+
+# def concatenate_string(df):
+#     return df.withColumn("modified_string",concat(df['title'],lit("h")))
+
+# def counter(df):
+#     x="ok"+df.title
+#     return df.withColumn("length_of_string",x)
+
+
+# df.transform(concatenate_string).transform(counter).display()
+
+The above example may have worked because pyspark would have converted it into arrays 
+
+# MOST IMPORTANT THING ABOUT THIS IS THAT WE NEED TO APPLY TRANSFORM ONLY ON THOSE COLUMNS THAT CONTAIN VALUES IN 
+# THE FORM OF ARRAYS.
+
+# What is a dataframe ?
+# A dataframe is a table like structure consisting of rows and columns.
+
+# Let us try to do it with arrays as well.
+
+data=[("A",[1,2,3,4]),("B",[5,6,7,8])]
+schema=["Name","ArrayColumn"]
 
 def concatenate_string(df):
-    return df.withColumn("modified_string",concat(df['title'],lit("h")))
+    return df.withColumn("concatenating_string",concat(df['ArrayColumn'],lit(' h')))
+    
+def len_string(df):
+    return df.withColumn("length_of_string",concat(df['ArrayColumn'],length(df['Name'])))
+    
 
-def counter(df):
-    x="ok"+df.title
-    return df.withColumn("length_of_string",x)
+df=spark.createDataFrame(data,schema)
+df.transform(concatenate_string).transform(len_string).display()
 
 
-df.transform(concatenate_string).transform(counter).display()
+
+
+
