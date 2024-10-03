91f19c52501a899b6af8ae7ac4516385835d5977
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-03 21:28:08 +0530

    Update map.py

--- RDD/map.py
+++ RDD/map.py
@@ -1,1 +1,8 @@
+# map function is used to apply a function to each and every value of the dataframe.
 
+def func(x):
+    x=x+"h"
+    return x
+
+rdd2=rdd.map(lambda x: func(x))
+print(rdd2.collect())
