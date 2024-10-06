f643ad5c004f3f5cb480d864246bd7b2e79b86c0
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-06 19:37:53 +0530

    Update 006) Expr concatenation.py

--- Built In Functions/006) Expr concatenation.py
+++ Built In Functions/006) Expr concatenation.py
@@ -1,5 +1,7 @@
 from pyspark.sql.functions import expr
 
+# Note that whenever we use expr , we need to use double quotes / single quotes
+
 df=spark.read.format("csv").option("header","true").load("dbfs:/FileStore/movies.csv")
 
 df1=df.withColumn("concatenated column",expr("movieId ||' '|| title"))
