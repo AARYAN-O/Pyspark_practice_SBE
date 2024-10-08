c68d1448e192e1925d0b52084e4d91a3a65976ed
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-08 15:57:55 +0530

    Update 051) Multiple Aggregates(using agg).py

--- Dataframes/051) Multiple Aggregates(using agg).py
+++ Dataframes/051) Multiple Aggregates(using agg).py
@@ -13,3 +13,12 @@ df.display()
 # Whenever you would want to use aggregations using agg() , i.e. whenever you would want to use :
 # sum,mean,avg,count,min & max , you should import them from pyspark.sql.functions because if you dont 
 # python will get confused if these are pyspark's functions or python's functions.
+
+# Multiple Aggregation Example
+
+from pyspark.sql.functions import col,count
+
+df=spark.read.format("csv").option("header","true").load("dbfs:/FileStore/superstore.csv")
+df.printSchema()
+df1=df.groupBy("City").agg(count("ShipMode"),sum("Discount"))
+df1.display()
