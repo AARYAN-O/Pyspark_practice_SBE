7ebae86e681c466f222c65e3e3f9ab9d4b3cb2ff
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-08 17:45:37 +0530

    Update 020) regexp_replace.py

--- Built In Functions/020) regexp_replace.py
+++ Built In Functions/020) regexp_replace.py
@@ -5,3 +5,6 @@ df=df.withColumn('genres',regexp_replace('genres','Adventure','Ad'))
 df.display()
 
 # Note : Do not use the wrong spelling as regex_replace
+
+# characters for replacement. If this is shorter than matching string then those chars that don’t have replacement 
+# will be dropped.
