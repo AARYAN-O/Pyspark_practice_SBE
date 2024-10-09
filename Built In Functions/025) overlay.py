5c495b34ce00e073a0b6f7231cc2d0858b40eb96
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-09 09:48:18 +0530

    Create 025) overlay.py

---
+++ Built In Functions/025) overlay.py
@@ -0,0 +1,8 @@
+from pyspark.sql.functions import overlay
+
+df=spark.read.format("csv").option("header","true").load("dbfs:/FileStore/movies.csv")
+
+df.select(overlay("title","movieID",2)).display()
+
+# Overlay helps us to overwrite value present in the movieID column over the value present inside title column after
+# the second character.
