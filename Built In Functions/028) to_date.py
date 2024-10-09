f94bb3b6234077761d1ff6ca2394fb3d71d8e33e
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-09 13:27:36 +0530

    Update 028) to_date.py

--- Built In Functions/028) to_date.py
+++ Built In Functions/028) to_date.py
@@ -10,4 +10,27 @@ df.printSchema()
 
 df=df.withColumn("new_column",to_date("date"))
 
-df.printSchema()
+df.printSchema
+
+# Note that there is something like timestamptype in pyspark too.
+# 1. StringType: For strings.
+# 2. BinaryType: For binary data (bytes).
+# 3. BooleanType: For boolean values (True or False).
+# 4. DateType: For dates (without time).
+# 5. TimestampType: For date and time values.
+# 6. DecimalType: For decimal numbers (with fixed precision and scale).
+# 7. DoubleType: For double-precision floating-point numbers.
+# 8. FloatType: For single-precision floating-point numbers.
+# 9. ByteType: For 8-bit signed integers.
+# 10. ShortType: For 16-bit signed integers.
+# 11. IntegerType: For 32-bit signed integers.
+# 12. LongType: For 64-bit signed integers.
+# 13. ArrayType: For arrays (lists) of a specific type.
+# 14. MapType: For key-value pairs, where both keys and values have specific types.
+# 15. StructType: For defining a schema with multiple fields (like a table schema).
+# 16. StructField: Represents a field in a StructType.
+# 17. NullType: For representing null values.
+# 18. CalendarIntervalType: For intervals of time (e.g., months, days, hours).
+# 19. TimestampNTZType: Timestamp without time zone.
+# 20. DayTimeIntervalType: Represents intervals of days, hours, minutes, and seconds.
+# 21. YearMonthIntervalType: Represents intervals of years and months.
