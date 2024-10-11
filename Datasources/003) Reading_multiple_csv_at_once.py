eecc594a99364356689880d29833393987998017
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-11 21:51:04 +0530

    Update 003) Reading_multiple_csv_at_once.py

--- Datasources/003) Reading_multiple_csv_at_once.py
+++ Datasources/003) Reading_multiple_csv_at_once.py
@@ -1,3 +1,5 @@
+# Note that here we need to put the list of strings
+
 df=spark.read.csv(["dbfs:/FileStore/world_bank_latest.csv","dbfs:/FileStore/movies.csv"])
 
 # this will merge the columns horizontally
