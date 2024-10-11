5a931dce349ead888551632ba1b65644f690a8da
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-11 20:44:58 +0530

    Update 030) date_format.py

--- Built In Functions/030) date_format.py
+++ Built In Functions/030) date_format.py
@@ -6,7 +6,27 @@ schema=[]
 df=spark.createDataFrame(data,schema)
 
 df=df.withColumn("new_column",date_format(current_timestamp(),"dd MM yyyy"))
+df.display()
 
+df=df.withColumn("new_column",date_format(current_timestamp(),"dd MM yy"))
+df.display()
+
+df=df.withColumn("new_column",date_format(current_timestamp(),"dd/MM/yyyy"))
+df.display()
+
+df=df.withColumn("new_column",date_format(current_timestamp(),"dd-MM-yyyy"))
+df.display()
+
+df=df.withColumn("new_column",date_format(current_timestamp(),"hh mm ss"))
+df.display()
+
+df=df.withColumn("new_column",date_format(current_timestamp(),"hh/mm/ss"))
+df.display()
+
+df=df.withColumn("new_column",date_format(current_timestamp(),"hh:mm:ss"))
+df.display()
+
+df=df.withColumn("new_column",date_format(current_timestamp(),"hh-mm-ss"))
 df.display()
 
 # Note : There is a difference between m and M in pyspark
