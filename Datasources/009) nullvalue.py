f2b26962858e2cb6f5b2d8ec8cc35cc58f018ca8
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-12 12:00:50 +0530

    Update 009) nullvalue.py

--- Datasources/009) nullvalue.py
+++ Datasources/009) nullvalue.py
@@ -7,3 +7,8 @@ df.display()
 # Note : Pyspark does not support multiple nullvalues..it supports only single nullvalue
 
 df=spark.read.format("csv").option("header","true").option("nullValue","NA").option("nullValue","NULL").load("dbfs:/FileStore/test_csv.csv")
+
+# Name	Age	City
+# John	25	"""""""New York"""""""
+# Jane	NA	"""""""San Francisco"""""""
+# Jake	null	"""""""Boston"""""""
