b67252a099826742f57d982a9749cad9306ed56d
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-12 10:41:54 +0530

    Update 005)Delimiters.py

--- Datasources/005)Delimiters.py
+++ Datasources/005)Delimiters.py
@@ -4,5 +4,8 @@
 # option (): This function can support only single attribute/operation but multiple option () function can be used in
 # series. options (): This function can support multiple attributes/operations using comma separated Key value pairs.
 
+# This means that inside option we can have only one option and then to add more options with "option" , we need to 
+# use spark.read.option().option().csv()
+
 df=spark.read.options(delimiter=",").csv("dbfs:/FileStore/movies.csv")
 df.display()
