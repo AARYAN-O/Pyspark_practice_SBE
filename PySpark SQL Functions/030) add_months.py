2f55153de0269473872181889598fb6f3a8e7539
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-12 21:01:19 +0530

    Create 030) add_months.py

---
+++ PySpark SQL Functions/030) add_months.py
@@ -0,0 +1,25 @@
+from pyspark.sql.functions import add_months,current_date,col
+
+data = (("James", "Sales", 3000), \
+    ("Michael", "Sales", 4600),  \
+    ("Robert", "Sales", 4100),   \
+    ("Maria", "Finance", 3000),  \
+    ("James", "Sales", 3000),    \
+    ("Scott", "Finance", 3300),  \
+    ("Jen", "Finance", 3900),    \
+    ("Jeff", "Marketing", 3000), \
+    ("Kumar", "Marketing", 2000),\
+    ("Saif", "Sales", 4100) \
+  )
+
+schema= ["employee_name", "department", "salary"]
+
+df=spark.createDataFrame(data,schema)
+
+df=df.withColumn("current_date",current_date())
+
+df.display()
+
+df=df.withColumn("added months",add_months(df["current_date"],1))
+
+df.display()
