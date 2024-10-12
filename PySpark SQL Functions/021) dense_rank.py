6783128d393b9bb8530565407ed58dd0e157a9f8
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-12 18:00:38 +0530

    Update 021) dense_rank.py

--- PySpark SQL Functions/021) dense_rank.py
+++ PySpark SQL Functions/021) dense_rank.py
@@ -9,4 +9,8 @@ df=df.withColumn("dense_rank",dense_rank().over(window_spec))
 
 df.display()
 
-https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiI-mTDNOHmxIAsyeLA45ctIwYpc8sat7cmcXqbed7u56dYwbqJQGn3suzDeK3POUIogb2ohhWDk_IRGuoyQxO6na_UkKxFosa598Rt1uUTrzKPWT8yiVr_iiLAP6YXDY8kLzCKK0arVwo/s1600/Slide4.PNG
+# https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiI-mTDNOHmxIAsyeLA45ctIwYpc8sat7cmcXqbed7u56dYwbqJQGn3suzDeK3POUIogb2ohhWDk_IRGuoyQxO6na_UkKxFosa598Rt1uUTrzKPWT8yiVr_iiLAP6YXDY8kLzCKK0arVwo/s1600/Slide4.PNG
+
+# dense_rank ==> ranking system in schools with ties 
+# row_number==> ranking system in school without ties
+# rank ==> wierd
