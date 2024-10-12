a5778881e7896bf8c0de871818f477d2899590ec
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-12 11:50:05 +0530

    Delete Built In Functions/009) nullvalue.py

--- Built In Functions/009) nullvalue.py
+++
@@ -1,9 +0,0 @@
-# nullvalue ensures that specified strings are treated as NULL only.
-
-df=spark.read.format("csv").option("header","true").option("nullValue","NA").load("dbfs:/FileStore/test_csv.csv")
-
-df.display()
-
-# Note : Pyspark does not support multiple nullvalues..it supports only single nullvalue
-
-df=spark.read.format("csv").option("header","true").option("nullValue","NA").option("nullValue","NULL").load("dbfs:/FileStore/test_csv.csv")
