778ec9fb08f64a36b8cd57ecb856f758803c9127
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-12 10:39:38 +0530

    Update 005)Delimiters.py

--- Datasources/005)Delimiters.py
+++ Datasources/005)Delimiters.py
@@ -1,5 +1,8 @@
 # By default , the delimiter is comma ; but you can set it to anything else as well.
 # Note that we need to use options instead of option here.
 
+# option (): This function can support only single attribute/operation but multiple option () function can be used in
+# series. options (): This function can support multiple attributes/operations using comma separated Key value pairs.
+
 df=spark.read.options(delimiter=",").csv("dbfs:/FileStore/movies.csv")
 df.display()
