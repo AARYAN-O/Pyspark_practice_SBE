43d2f8efdc389c63208babfe061698d16b0228fa
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-12 15:24:49 +0530

    Update 013) dbutils(fs.mkdirs).py

--- Datasources/013) dbutils(fs.mkdirs).py
+++ Datasources/013) dbutils(fs.mkdirs).py
@@ -1,4 +1,4 @@
 dbutils.fs.mkdirs("dbfs:/FileStore/new_directory")
 
 # creating a new directory 
-# If the directory is successfully made , it is mkdir
+# If the directory is successfully made , it will return true , otherwise it will return false.
