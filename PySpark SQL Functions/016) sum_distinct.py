d6ad86eb7566f288629ede0548c865202b600c9b
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-12 17:08:47 +0530

    Update 016) sum_distinct.py

--- PySpark SQL Functions/016) sum_distinct.py
+++ PySpark SQL Functions/016) sum_distinct.py
@@ -1,3 +1,5 @@
+# sum_distinct gives the sum of the distinct values inside the column only
+
 from pyspark.sql.functions import sum_distinct
 
 df=spark.read.format("csv").option("header","true").load("dbfs:/FileStore/movies.csv")
