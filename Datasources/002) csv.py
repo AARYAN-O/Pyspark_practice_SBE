73bc4de0d9772839d74ef473d3978110a129fc2c
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-12 13:54:23 +0530

    Update 002) csv.py

--- Datasources/002) csv.py
+++ Datasources/002) csv.py
@@ -1,2 +1,4 @@
+# Note : When you want to upload the file , we need to go to dbfs and click on upload and then browse.
+
 df=spark.read.option("header","true").csv("dbfs:/FileStore/world_bank_latest.csv")
 df.display()
