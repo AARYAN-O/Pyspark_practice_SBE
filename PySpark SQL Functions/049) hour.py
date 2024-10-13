384e9693cf00d4edde2a936b272d59349bb2e441
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-13 10:48:22 +0530

    Create 049) hour.py

---
+++ PySpark SQL Functions/049) hour.py
@@ -0,0 +1,21 @@
+from pyspark.sql.functions import hour, current_timestamp
+
+data = (("James", "Sales", 3000), \
+    ("Michael", "Sales", 4600),  \
+    ("Robert", "Sales", 4100),   \
+    ("Maria", "Finance", 3000),  \
+    ("James", "Sales", 3000),    \
+    ("Scott", "Finance", 3300),  \
+    ("Jen", "Finance", 3900),    \
+    ("Jeff", "Marketing", 3000), \
+    ("Kumar", "Marketing", 2000),\
+    ("Saif", "Sales", 4100) \
+  )
+
+schema= ["employee_name", "department", "salary"]
+
+df=spark.createDataFrame(data,schema)
+
+df=df.withColumn("current_timestamp",current_timestamp()).withColumn("hour",hour(current_timestamp()))
+
+df.display()
