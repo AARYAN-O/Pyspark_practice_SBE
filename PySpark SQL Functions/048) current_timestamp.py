11cfd2c1386ee0781fede9aebd49aaa9a1eb536e
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-13 10:43:49 +0530

    Create 048) current_timestamp.py

---
+++ PySpark SQL Functions/048) current_timestamp.py
@@ -0,0 +1,21 @@
+from pyspark.sql.functions import current_timestamp
+
+data = (("James", "Sales", 3000), \
+    ("Michael", "Sales", 4600),  \
+    ("Robert", "Sales", 4100),   \
+    ("Maria", "Finance", 3000),  \
+    ("James", "Sales", 3000),    \
+    ("Scott", "Finance", 3300),  \
+    ("Jen", "Finance", 3900),    \
+    ("Jeff", "Marketing", 3000), \
+    ("Kumar", "Marketing", 2000),\
+    ("Saif", "Sales", 4100) \
+  )
+
+schema= ["employee_name", "department", "salary"]
+
+df=spark.createDataFrame(data,schema)
+
+df=df.withColumn("timestamp column",current_timestamp())
+
+df.display()
