8eb83077decf78e0981c972a33c82c9dea62608a
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-13 13:09:29 +0530

    Update 053) from_json.py

--- PySpark SQL Functions/053) from_json.py
+++ PySpark SQL Functions/053) from_json.py
@@ -15,7 +15,9 @@ df=df.withColumn("json_col",from_json(df.value,MapType(StringType(),StringType()
 df.display()
 
 # from json function is used to convert the json string into structType or MapType.
-
-
+# Note : In python , we can convert almost any datatype into string.
+# MapType is used to tell the datatypes of the key-value pairs that are present inside the json string
+# In json string , the key can be of one datatype and the value can be of other datatype
+# When there are multiple datatypes as values inside json string , its better to write them as stringType.
 
 
