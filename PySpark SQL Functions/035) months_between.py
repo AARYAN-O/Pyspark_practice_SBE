a8222a350e634e07d01e25015e587072d377105b
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-13 08:40:54 +0530

    Create 035) months_between.py

---
+++ PySpark SQL Functions/035) months_between.py
@@ -0,0 +1,23 @@
+from pyspark.sql.functions import months_between
+
+data = (("James", "Sales", 3000), \
+    ("Michael", "Sales", 4600),  \
+    ("Robert", "Sales", 4100),   \
+    ("Maria", "Finance", 3000),  \
+    ("James", "Sales", 3000),    \
+    ("Scott", "Finance", 3300),  \
+    ("Jen", "Finance", 3900),    \
+    ("Jeff", "Marketing", 3000), \
+    ("Kumar", "Marketing", 2000),\
+    ("Saif", "Sales", 4100) \
+  )
+
+schema= ["employee_name", "department", "salary"]
+
+df=spark.createDataFrame(data,schema)
+
+df=df.withColumn("date_1",current_date()+1).withColumn("date_2",current_date())
+
+df=df.withColumn("months_between",months_between("date_1","date_2"))
+
+df.display()
