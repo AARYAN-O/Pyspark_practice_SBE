3f58838d3f510ba69af10e610a34e26055f2d109
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-13 16:14:38 +0530

    Update 017) overwrite_in_parquet.py

--- Datasources/017) overwrite_in_parquet.py
+++ Datasources/017) overwrite_in_parquet.py
@@ -5,3 +5,5 @@ df.write.mode("overwrite").parquet("dbfs:/output/sales_us/new_users.parquet")
 df1=spark.read.format("parquet").load("dbfs:/output/sales_us/new_users.parquet")
 
 df1.display()
+
+# overwrite option will check if something exists in the path of the file, if it exists it gets overwritten .
