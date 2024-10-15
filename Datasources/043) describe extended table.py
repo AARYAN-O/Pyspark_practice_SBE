7c7a3ae9cb92cc63907b613c001373fad71aa988
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-15 19:00:59 +0530

    Update 043) describe extended table.py

--- Datasources/043) describe extended table.py
+++ Datasources/043) describe extended table.py
@@ -1,1 +1,4 @@
+# describe extended table is used to get more information on the table .This could be regarding the type of the format
+# of the table
+
 spark.sql("describe extended my_managed_table").display()
