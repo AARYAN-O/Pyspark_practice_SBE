3ecb8982d1498191effb5fb385ca41d25e6fbb63
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-15 18:31:24 +0530

    Update 038) Storing_Data_inside_db_within_hive.py

--- Datasources/038) Storing_Data_inside_db_within_hive.py
+++ Datasources/038) Storing_Data_inside_db_within_hive.py
@@ -7,3 +7,6 @@ spark.read.table("temp_table").display()
 
 # What if we want to store the data inside a particular database within hive.
 # /user/hive/warehouse/db1.db/temp_table
+
+# Note : using spark.sql() , the data gets stored by default in the parquet format.
+# parquet is columnar storage file and is used for data processing usually
