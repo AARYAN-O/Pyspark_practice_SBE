15ddd415440dafab20cd0c797fb1606733595dd5
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-15 14:44:04 +0530

    Create 038) Storing_Data_inside_db_within_hive.py

---
+++ Datasources/038) Storing_Data_inside_db_within_hive.py
@@ -0,0 +1,9 @@
+spark.sql("create database db2")
+
+df.write.mode("overwrite").saveAsTable("db2.temp_table")
+
+spark.read.table("temp_table").display()
+
+
+# What if we want to store the data inside a particular database within hive.
+# /user/hive/warehouse/db1.db/temp_table
