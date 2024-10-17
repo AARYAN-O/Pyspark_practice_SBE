777182109689697f05f02c94d10e9b430f73a55d
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-17 17:00:24 +0530

    Update 001) map.py

--- RDD/001) map.py
+++ RDD/001) map.py
@@ -1,6 +1,9 @@
 # map function is used to apply a function to each and every value of the dataframe.
 # Note : The map() can be used for only rdds and not dataframes
 
+data=['a','b']
+rdd=spark.sparkContext.parallelize(data)
+
 def func(x):
     x=x+"h"
     return x
