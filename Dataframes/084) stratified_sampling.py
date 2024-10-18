95721595d1c7be6dd9b89e85860ac2bcc8a32e17
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-18 16:22:34 +0530

    Update 084) stratified_sampling.py

--- Dataframes/084) stratified_sampling.py
+++ Dataframes/084) stratified_sampling.py
@@ -1,1 +1,6 @@
+# Stratified random sampling is a method of selecting a sample in which researchers first divide a population into smaller
+# subgroups, or strata, based on shared characteristics of the members and then randomly select among each stratum to form 
+# the final sample.
+
+# This can be done using sampleBy()
 
