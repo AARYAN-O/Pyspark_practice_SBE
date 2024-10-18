6239f571c7565f115987f79581ef8cde7395f5db
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-18 19:27:27 +0530

    Create 089) na.fill_or_fillna.py

---
+++ Dataframes/089) na.fill_or_fillna.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import pandas as pd 
+
+post_url='http://universities.hipolabs.com/search?country=United+States'
+
+response=requests.get(post_url)
+
+
+data=response.json()
+# The data is a  list
+
+df=pd.DataFrame(data)
+# pd.DataFrame is a dataframe 
+
+df=spark.createDataFrame(df)
+
+df.display()
+
+df=df.fillna(value='unknown',subset=['state-province'])
+
+# Note that the subset can be a list. Hence we can apply that logic to multiple columns.
+# Even if you remove the list , everything will work fine 
+
+df=df.fillna(value='unknown',subset='state-province')
+
+print("new dataframe")
+
+df.display()
