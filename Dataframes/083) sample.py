dbcc7e9f61701a7ea88c6548045c4c01a031e2e7
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-18 16:10:14 +0530

    Update 083) sample.py

--- Dataframes/083) sample.py
+++ Dataframes/083) sample.py
@@ -11,3 +11,20 @@ df=pd.DataFrame(data)
 df=spark.createDataFrame(df)
 
 df.sample(0.1).display()
+
+# sample with replacement 
+
+df.sample(True,0.1).display()
+
+# sample(withReplacement, fraction, seed=None) --> fraction does not gurantee exact number of fractions 
+
+# What is the use of seed ? --> seed is used to generate the same samples. If we have same seed number , 
+# it means that we will get random number.
+
+df.sample(True,0.2,123) 
+
+ # setting seed as 123
+
+# Note that it doesn’t guarantee to provide the exact number of the fraction of records.
+
+# A random seed (or seed state, or just seed) is a number (or vector) used to initialize a pseudorandom number generator
