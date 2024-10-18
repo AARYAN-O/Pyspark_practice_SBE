debec9c88f4fd0a8b8a8d98181fc2d3c8d5a795e
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-18 13:42:54 +0530

    Create 081) foreach.py

---
+++ Dataframes/081) foreach.py
@@ -0,0 +1,5 @@
+# Note that it works on each and every row and computes row wise.
+# forrach statement does not return anything.
+# Note that whenever there is distributed computing , we will be having multiple rows distributed across various worker 
+# nodes and hence to handle data across various worker nodes , we use accumulators.
+
