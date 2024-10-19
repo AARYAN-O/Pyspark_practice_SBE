e0b91b259de13afaba75bd8f8cfc287bc2858200
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-19 17:55:05 +0530

    Update 091) Replacing_multiple_nulls_with_some_other_value.py

--- Dataframes/091) Replacing_multiple_nulls_with_some_other_value.py
+++ Dataframes/091) Replacing_multiple_nulls_with_some_other_value.py
@@ -10,11 +10,26 @@ df.display()
 
 # using na.fill()
 
-
 df=spark.read.format("csv").option("header","true").load("dbfs:/FileStore/bank_customer.csv")
 
 df.display()
 
 df=df.na.fill("unknown","Initials").na.fill("unknown","cust_lname")
+
+df.display()
+
+# using dictionaries 
+
+# The below lines of code prove the fact that dictionaries can be used for both na.fill and fillna
+
+df=spark.read.format("csv").option("header","true").load("dbfs:/FileStore/bank_customer.csv")
+
+df.display()
+
+df=df.na.fill({"initials":"unknown","cust_lname":"unknown"})
+
+df.display()
+
+df=df.fillna({"initials":"unknown","cust_lname":"unknown"})
 
 df.display()
