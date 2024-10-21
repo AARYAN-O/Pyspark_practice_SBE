6684fbf342c3f577803cec56ec6a6b7e07d55128
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-21 12:20:15 +0530

    Create 107) map_keys.py

---
+++ Dataframes/107) map_keys.py
@@ -0,0 +1,17 @@
+# This function is used to get all the keys present in the dictionary in the format of a list.
+
+from pyspark.sql.types import StructType,StructField,MapType,StringType
+from pyspark.sql.functions import map_keys
+
+schema=StructType([StructField("name",StringType(),True),StructField("properties",MapType(StringType(),StringType()),True)])
+data=[("A",{"color":"red","year":"2000"}),("B",{"color":"black","return on investment":200000})]
+
+df=spark.createDataFrame(data,schema)
+
+df.display()
+
+df=df.select("name",map_keys(df.properties))
+
+df.display()
+
+# The above will return the functions values as properties mapped with the name.
