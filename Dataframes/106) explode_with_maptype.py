880328492860648dff35622fe34578ec78367098
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-21 11:53:28 +0530

    Create 106) explode_with_maptype.py

---
+++ Dataframes/106) explode_with_maptype.py
@@ -0,0 +1,16 @@
+# How does explode works on dictionaries?
+# explode separates key values pairs in the dictionaries into two separate columns.
+
+from pyspark.sql.types import StructType,StructField,MapType,StringType
+from pyspark.sql.functions import explode
+
+schema=StructType([StructField("name",StringType(),True),StructField("properties",MapType(StringType(),StringType()),True)])
+data=[("A",{"color":"red","year":"2000"}),("B",{"color":"black","return on investment":200000})]
+
+df=spark.createDataFrame(data,schema)
+
+df.display()
+
+df=df.select("name",explode(df.properties))
+
+df.display()
