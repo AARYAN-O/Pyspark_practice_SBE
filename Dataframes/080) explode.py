26a83c733082671b6a919298ebc6da6c3dd7f743
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-10-21 13:54:02 +0530

    Update 080) explode.py

--- Dataframes/080) explode.py
+++ Dataframes/080) explode.py
@@ -7,3 +7,19 @@ df=spark.createDataFrame(data,schema)
 df=df.withColumn("exploded",explode(df.list_of_characters))
 
 df.display()
+
+# Note : explode will create mapping of all the columns (like cross joins) that are included with the first column 
+# this will be valid in case , we are using something like this 
+
+# +----------------+------+
+# |            name|   col|
+# +----------------+------+
+# |    James,,Smith|  Java|
+# |    James,,Smith| Scala|
+# |    James,,Smith|   C++|
+# |   Michael,Rose,| Spark|
+# |   Michael,Rose,|  Java|
+# |   Michael,Rose,|   C++|
+# |Robert,,Williams|CSharp|
+# |Robert,,Williams|    VB|
+# +----------------+------
